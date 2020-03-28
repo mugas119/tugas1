daa17d9c907f41a60ef570816faf812290fd6ebb
mugas119 <62794984+mugas119@users.noreply.github.com>
2020-03-28 18:21:17 +0700

    Add files via upload

---
+++ modules/beranda/views/v_beranda.php
@@ -0,0 +1,16 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed')?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>
+            Beranda | Tutorial Simple Login CodeIgniter
+        </title>
+    </head>
+    <body>
+        <h1>Welcome to our Site</h1>
+        <p>silahkan klik <?php echo anchor('login','masuk');?>untuk masuk kedalam sistem atau <?php echo anchor('register','daftar');?>untuk mendaftar
+</p>
+    </body>
+</html>
